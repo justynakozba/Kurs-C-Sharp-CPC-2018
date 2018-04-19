559f72497189b8980fe12c3e0533a2cffcb9218d
Justyna <justynakozba@gmail.com>
2018-04-19 21:17:14 +0200

    Dodanie zadania wykonanego na zajęciach

---
+++ lab3/Zad0/JustynaKozbaLab3/JustynaKozbaLab3/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace JustynaKozbaLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonShow1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelName.Location = new System.Drawing.Point(11, 24);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(340, 55);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Justyna Koźba";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(517, 311);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(544, 82);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(103, 42);
+            this.buttonShow.TabIndex = 2;
+            this.buttonShow.Text = "Show";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(547, 168);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonShow1
+            // 
+            this.buttonShow1.Location = new System.Drawing.Point(544, 217);
+            this.buttonShow1.Name = "buttonShow1";
+            this.buttonShow1.Size = new System.Drawing.Size(124, 35);
+            this.buttonShow1.TabIndex = 4;
+            this.buttonShow1.Text = "Show";
+            this.buttonShow1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 405);
+            this.Controls.Add(this.buttonShow1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelName);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonShow1;
+    }
+}
+
---
+++ lab3/Zad0/JustynaKozbaLab3/JustynaKozbaLab3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace JustynaKozbaLab3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(@"Data Source=DESKTOP-VR1D9PV; database= Pzzeria; Trusted_Connection=yes");
+        }
+
+        /// <summary>
+        /// Metoda na kliknięcie przycisku 1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM Pizzas WHERE Price > 0{textBox1.Text}", connection);
+            DataTable table = new DataTable();
+
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
