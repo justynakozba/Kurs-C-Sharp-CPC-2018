748cb06b92a041e110188c565c89a47ebcf9ca70
Justyna <justynakozba@gmail.com>
2018-06-07 20:58:58 +0200

    Dodanie zadania wykonanego na zajęciach lab7.

---
+++ lab7/JustynaKozbaLab7/JustynaKozbaLab7/script.js
@@ -0,0 +1,20 @@
+﻿document.getElementById("text-information").style.backgroundColor = "deeppink";
+var counter = 1;
+function changeFooterTextAndColor() {
+    document.getElementById("footer-block").style.backgroundColor = "yellow";
+    document.getElementById("footer-block").innerHTML = "Licznik: " + counters;
+    counter++;
+}
+
+function changeBanner() {
+    document.getElementById("banner-element").style.width = "300px";
+    alert("zadziałało!");
+}
+
+/*Ustawienie zdarzenia cyklicznego w zależności od czasu*/
+setInterval(changeFooterTextAndColor, 1000)
+
+
+
+
+
