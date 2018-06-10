2a7653b0f0b2eb68e9d20f57f2953cee37a75818
Justyna <justynakozba@gmail.com>
2018-06-10 18:30:02 +0200

    Oddanie zadania domowego lab7.
    Oddanie początkowej wersji zadania domowego z podstawowymi
    funkcjonalnościami.

---
+++ lab7/JustynaKozbaLab7Zad1/JustynaKozbaLab7Zad1/scripts/script.js
@@ -0,0 +1,22 @@
+﻿
+/*Funkcja zmieniająca kolory stopki*/
+var counter = 1;
+function changeFooterTextAndColor() {
+
+    
+    if (document.getElementById("footer-block").style.backgroundColor == "cyan") {
+        document.getElementById("footer-block").style.backgroundColor = "blue";
+    }
+    else if (document.getElementById("footer-block").style.backgroundColor == "blue") {
+        document.getElementById("footer-block").style.backgroundColor = "green";
+    }
+    else
+    {
+        document.getElementById("footer-block").style.backgroundColor = "cyan";
+    }
+    document.getElementById("footer-block").innerHTML = "Licznik: " + counters;
+    counter++;
+}
+
+/*Ustawienie zdarzenia cyklicznego w zależności od czasu*/
+setInterval(changeFooterTextAndColor, 1000)
